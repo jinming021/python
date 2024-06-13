2737dcd1fdb176015f781cab132b3f6cb63ea668
jinming021 <jinming021@gmail.com>
2024-06-14 04:36:49 +0800

    list_VII

--- python_test.py
+++ python_test.py
@@ -1,28 +1,13 @@
-"""
-1. 请编程找出矩阵中的幸运数字.
-说明：假设给定一个 m * n 的矩阵（矩阵中数值的取值范围是 0~1024，且各不相同），
-如果某一个元素的值在同一行的所有元素中最小，并且在同一列的所有元素中最大，那么该元素便是幸运数字。
-假设给定的矩阵如下：
-matrix = [[10, 36, 52],
-          [33, 24, 88],
-          [66, 76, 99]]
-          
-那么输出结果应该是 66（同时满足同一行的所有元素中最小，并且在同一列的所有元素中最大）。
-"""
+def print_yanghui_triangle(n):
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
 
-import random
-matrix = []
+    for row in triangle:
+        print(row)
 
-for m in range(3):
-    matrix.append([])
-    for n in range(3):
-        matrix[m].append(random.randint(0,1024))
-print(matrix)
-
-for m in range(3):
-    min_num = min(matrix[m])
-    max_num = max(matrix[i][m] for i in range(3))
-    if min_num == max_num:
-        print(min_num)
-
-
+print_yanghui_triangle(10)
