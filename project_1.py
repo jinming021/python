4780e4be672fc8ced712d5c97cf547d8ed31b8fe
jinming021 <jinming021@gmail.com>
2024-06-18 03:23:40 +0800

    string

--- project_1.py
+++ project_1.py
@@ -48,10 +48,11 @@ print("Case1: 5GC000433_01_Feature_Actiovation")
 print("Step1: login to WebUI")
 
 driver = webdriver.Edge()
-driver.get("http://www.python.org")
+driver.get("https://10.101.48.132/")
 
 
 print("Step2: Activate 5GC000433 feature")
 
 print("Step3: Waiting for 15 minutes, check if PM counter is updated")
 print("Step4: Collect Log and analyze")
+
