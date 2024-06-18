94914628078245487bd30ba59133c2b156559c50
jinming021 <jinming021@gmail.com>
2024-06-19 06:40:08 +0800

    str

--- python_test.py
+++ python_test.py
@@ -1,13 +1,20 @@
-def print_yanghui_triangle(n):
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
+#编写一个生成凯撒密码的程序
+plain_text = input("请输入需要加密的明文（只支持英文字母）：")
+shift = int(input("请输入移动的位数："))
 
-    for row in triangle:
-        print(row)
+crypt_text = ""
 
-print_yanghui_triangle(10)
+for char in plain_text:
+    if char.isalpha():
+        if char.isupper():
+            crypt_text += chr((ord(char) - ord("A") + shift) % 26 + ord("A"))
+        elif char.islower():
+            crypt_text += chr((ord(char) - ord("a") + shift) % 26 + ord("a"))
+        else:
+            crypt_text = char
+    else:
+        crypt_text += char
+
+print(crypt_text)
+
+
